27de8f7ccb77a1dc2296f0cbbd1adf841a81ffe0
xinxu <xin872906473@163.com>
2022-07-06 10:37:48 +0800

    00011-easy-tuple-to-object

---
+++ type-challenges/00011-easy-tuple-to-object/template.ts
@@ -0,0 +1,31 @@
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P;
+};
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+// keyof array  -> 索引
+type r = TupleToObject<typeof tuple>;
+
+// js
+
+function tupleToObject(array) {
+  //  array 里面的key不是普通类型就报错
+  const isError = array.some((i) => {
+    const type = typeof i;
+    if (!["string", "number", "symbol"].includes(type)) {
+      return true;
+    }
+    return false;
+  });
+  if (isError) {
+    throw new Error("");
+  }
+  const obj = {};
+  array.forEach((element) => {
+    obj[element] = element;
+  });
+  return obj;
+}
+
+// 1. 返回一个对象
+// 2. 遍历array
---
+++ type-challenges/00011-easy-tuple-to-object/test-cases.ts
@@ -0,0 +1,41 @@
+import type { Equal, Expect } from "@type-challenges/utils";
+
+const tuple = ["tesla", "model 3", "model X", "model Y"] as const;
+const tupleNumber = [1, 2, 3, 4] as const;
+const tupleMix = [1, "2", 3, "4"] as const;
+
+type cases = [
+  Expect<
+    Equal<
+      TupleToObject<typeof tuple>,
+      {
+        tesla: "tesla";
+        "model 3": "model 3";
+        "model X": "model X";
+        "model Y": "model Y";
+      }
+    >
+  >,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<
+    Equal<TupleToObject<typeof tupleMix>, { 1: 1; "2": "2"; 3: 3; "4": "4" }>
+  >
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
+
+// const let js 世界
+// type interface type 世界
+
+// type r = typeof tuple
+// // 1. 字面量类型
+// let str = '123'
+
+// type s =  typeof str
+
+// const strConst = '1233'
+// type sc = typeof strConst
+
+// 1. typeof js->ts
+// 2. 字面量类型 as const
