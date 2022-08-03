1e578aed73f97bfde3e56f09e3df919311a6b7dd
xinxu <xin872906473@163.com>
2022-08-03 19:48:03 +0800

    00108-medium-trim

--- type-challenges/00106-medium-trimleft/template.ts
+++ type-challenges/00106-medium-trimleft/template.ts
@@ -5,9 +5,3 @@ type TrimLeft<S extends string> = S extends `${" " | "\n" | "\t"}${infer R}`
 type TrimRight<S extends string> = S extends `${infer R}${" " | "\n" | "\t"}`
   ? TrimRight<R>
   : S;
-
-type Trim<S extends string> = S extends
-  | `${" " | "\n" | "\t"}${infer R}`
-  | `${infer R}${" " | "\n" | "\t"}`
-  ? Trim<R>
-  : S;
